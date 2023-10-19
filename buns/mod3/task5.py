1db631d62fc43ff746333084732427b2292867ec
alivkas <128307393+alivkas@users.noreply.github.com>
2023-10-19 16:50:04 +0500

    Update task5.py

--- buns/mod3/task5.py
+++ buns/mod3/task5.py
@@ -1,3 +1,3 @@
-stringNums = input("Введите строку ")
-list = ["yes" if stringNums.count("0") == stringNums.count("1") else "no"]
+string_nums = input("Введите строку ")
+list = ["yes" if string_nums.count("0") == string_nums.count("1") else "no"]
 print(list[0])
